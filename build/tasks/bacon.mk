LineageOS/android_vendor_lineage

bacon: Make it possible to let other build targets depend on lineage OTA package
Change-Id: I04ed91b62beed9728c354ea73f7c0baa72213808

build/tasks/bacon.mk
@@ -20,8 +20,10 @@ LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/lineage-$(LINEAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
-.PHONY: bacon
-bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
+$(LINEAGE_TARGET_PACKAGE): $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
+
+.PHONY: bacon
+bacon: $(LINEAGE_TARGET_PACKAGE) $(DEFAULT_GOAL)

Libra420T/vendor_infinity

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

build/tasks/bacon.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 # -----------------------------------------------------------------
-# Infinity OTA update package
 
+# Infinity OTA update package
 INFINITY_TARGET_PACKAGE := $(PRODUCT_OUT)/$(INFINITY_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
@@ -33,3 +33,7 @@ bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	echo -e ${CL_BLD}${CL_RED}"                    Thanks for trying out Project Infinity X ❤️"${CL_RED}
 	echo ""
 	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
+
+.PHONY: bacon
+bacon: $(INFINITY_TARGET_PACKAGE) $(DEFAULT_GOAL)
+